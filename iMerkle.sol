a67a70572e650e0a379fb4472447c17346a1820d
obingo31 <obrien@makenzi.cloud>
2024-08-05 11:57:45 +0100

    iMerkle.sol

---
+++ iMerkle.sol
@@ -0,0 +1,105 @@
+library Merkle {
+ function verifyInclusionKeccak(bytes memory proof, bytes32 root, bytes32 leaf, uint256 index)
+        internal
+        pure
+        returns (bool)
+    {
+        return processInclusionProofKeccak(proof, leaf, index) == root;
+    }
+function processInclusionProofKeccak(bytes memory proof, bytes32 leaf, uint256 index)
+        internal
+        pure
+        returns (bytes32)
+    {
+        require(proof.length % 32 == 0, "Merkle.processInclusionProofKeccak: proof length should be a multiple of 32");
+        bytes32 computedHash = leaf;
+        for (uint256 i = 32; i <= proof.length; i += 32) {
+            if (index % 2 == 0) {
+                // if ith bit of index is 0, then computedHash is a left sibling
+                assembly {
+                    mstore(0x00, computedHash)
+                    mstore(0x20, mload(add(proof, i)))
+                    computedHash := keccak256(0x00, 0x40)
+                    index := div(index, 2)
+                }
+            } else {
+                // if ith bit of index is 1, then computedHash is a right sibling
+                assembly {
+                    mstore(0x00, mload(add(proof, i)))
+                    mstore(0x20, computedHash)
+                    computedHash := keccak256(0x00, 0x40)
+                    index := div(index, 2)
+                }
+            }
+        }
+        return computedHash;
+    }
+ function verifyInclusionSha256(bytes memory proof, bytes32 root, bytes32 leaf, uint256 index)
+        internal
+        view
+        returns (bool)
+    {
+        return processInclusionProofSha256(proof, leaf, index) == root;
+    }
+function processInclusionProofSha256(bytes memory proof, bytes32 leaf, uint256 index)
+        internal
+        view
+        returns (bytes32)
+    {
+        require(
+            proof.length != 0 && proof.length % 32 == 0,
+            "Merkle.processInclusionProofSha256: proof length should be a non-zero multiple of 32"
+        );
+        bytes32[1] memory computedHash = [leaf];
+        for (uint256 i = 32; i <= proof.length; i += 32) {
+            if (index % 2 == 0) {
+                // if ith bit of index is 0, then computedHash is a left sibling
+                assembly {
+                    mstore(0x00, mload(computedHash))
+                    mstore(0x20, mload(add(proof, i)))
+                    if iszero(staticcall(sub(gas(), 2000), 2, 0x00, 0x40, computedHash, 0x20)) { revert(0, 0) }
+                    index := div(index, 2)
+                }
+            } else {
+                // if ith bit of index is 1, then computedHash is a right sibling
+                assembly {
+                    mstore(0x00, mload(add(proof, i)))
+                    mstore(0x20, mload(computedHash))
+                    if iszero(staticcall(sub(gas(), 2000), 2, 0x00, 0x40, computedHash, 0x20)) { revert(0, 0) }
+                    index := div(index, 2)
+                }
+            }
+        }
+        return computedHash[0];
+    }
+
+    /**
+     * @notice this function returns the merkle root of a tree created from a set of leaves using sha256 as its hash function
+     *  @param leaves the leaves of the merkle tree
+     *  @return The computed Merkle root of the tree.
+     *  @dev A pre-condition to this function is that leaves.length is a power of two.  If not, the function will merkleize the inputs incorrectly.
+     */
+    function merkleizeSha256(bytes32[] memory leaves) internal pure returns (bytes32) {
+        //there are half as many nodes in the layer above the leaves
+        uint256 numNodesInLayer = leaves.length / 2;
+        //create a layer to store the internal nodes
+        bytes32[] memory layer = new bytes32[](numNodesInLayer);
+        //fill the layer with the pairwise hashes of the leaves
+        for (uint256 i = 0; i < numNodesInLayer; i++) {
+            layer[i] = sha256(abi.encodePacked(leaves[2 * i], leaves[2 * i + 1]));
+        }
+        //the next layer above has half as many nodes
+        numNodesInLayer /= 2;
+        //while we haven't computed the root
+        while (numNodesInLayer != 0) {
+            //overwrite the first numNodesInLayer nodes in layer with the pairwise hashes of their children
+            for (uint256 i = 0; i < numNodesInLayer; i++) {
+                layer[i] = sha256(abi.encodePacked(layer[2 * i], layer[2 * i + 1]));
+            }
+            //the next layer above has half as many nodes
+            numNodesInLayer /= 2;
+        }
+        //the first node in the layer is the root
+        return layer[0];
+    }
+}
